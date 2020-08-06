1f520951b2b30848a1346bc6b9a37235da9236a1
Jack <63446360+jacklovesusu@users.noreply.github.com>
2020-08-06 16:12:11 +0800

    Create hashTable.py

---
+++ hashTable.py
@@ -0,0 +1,13 @@
+#散列表
+voted = {}
+def ifVoted(name):
+	if voted.get(name) == True:
+		print(name,'Have voted, let them out!')
+	else:
+		voted[name] = True
+		print('Let them in and vote!')
+
+ifVoted('Tom')
+ifVoted('Mike')
+ifVoted('Tom')
+ifVoted('Tim')
